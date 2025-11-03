d6a2fd662f7460aded77a50fdf87bd957487a18a
Marek Brzezinski <marek.j.brzezinski@gmail.com>
2025-11-03 13:05:10 +0100

    Added 2 functions for testing

---
+++ main.go
@@ -0,0 +1,14 @@
+// Package docexample helps to test documentation generation functions
+package docexample
+
+import "fmt"
+
+// TestFunction prints "Testing" to the console
+func TestFunction() {
+	fmt.Println("Testing")
+}
+
+// SecondFunction prints "Second" to the console
+func SecondFunction() {
+	fmt.Println("Second")
+}
